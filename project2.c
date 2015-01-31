1a15d1f7c94d9749608dc84c6c20efb2bbf96a5d
bkoning23 <koningb@mail.gvsu.edu>
2015-01-31 16:48:31 -0500

    initil commit, process rename working

---
+++ project2.c
@@ -0,0 +1,114 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <string.h>
+
+void sigHandler (int);
+void createServer(int, int, char*, char*[]);
+pid_t pid, secondPid, ppid;
+
+char str[50];
+char *word;
+int minProc, maxProc;
+int var2;
+
+
+int main(int argc, char *argv[])
+{
+	printf("Specify Command:\n");
+	fgets(str, 50, stdin);
+	word = strtok(str, " \n");
+	
+	if(!strcmp(word, "createServer")){
+		if ((pid = fork()) < 0){
+			perror("Fork failure");
+			exit(1);
+		}
+		else if (!pid){
+			word = strtok(NULL, " \n");
+			sscanf (word, "%d", &minProc);
+			word = strtok(NULL, " \n");
+			sscanf (word, "%d", &maxProc);		
+			word = strtok(NULL, " \n");
+			printf("Pid: %d\n", getpid());
+			createServer(minProc, maxProc, word, argv);
+		}
+		else{
+			sleep(2);
+			printf("Pid: %d\n", getpid());
+		}
+		
+	}
+
+	return(0);
+		
+
+}
+
+
+
+
+void createServer(int minProcs, int maxProcs, char *serverName, char *argv[]){
+	//Add checking that min is less than max
+	//TODO: This still doesn't work right
+	if(minProcs >= maxProcs){
+		perror("Min procs must be less than max procs");
+		exit(0);
+	}
+	
+	int i = 0;
+	while ( i != minProcs){
+		if ((pid = fork()) < 0){
+			perror("Fork error");
+		}
+		else if(!pid){
+			sprintf(argv[0], "Spawned Server %d", i);
+			break;
+		}
+		else{
+			i++;
+		}
+	}
+			
+	printf("Pid: %d\n", getpid());	
+	sleep(20);
+	
+	//printf("%d %d %s\n", minProcs, maxProcs, serverName);
+
+
+}	
+
+
+
+
+
+/*
+void
+sigHandler (int sigNum)
+{
+	if(sigNum == SIGUSR1){
+		printf("received a SIGUSR1 signal.\n");
+		signal(SIGUSR1, sigHandler);
+	}
+	if(sigNum == SIGUSR2){
+		printf("received a SIGUSR2 signal.\n");
+		signal(SIGUSR2, sigHandler);
+	}
+	if(sigNum == SIGINT){
+		//If the child receives the signal it does nothing. 
+		if (!firstPid){				
+		}
+		//Parent prints that it is closing and kills the child process 
+		else{
+			printf("received. Closing. \n");
+			kill(firstPid, SIGKILL);
+			exit(0);
+		}
+	}
+	
+    
+} 
+*/
