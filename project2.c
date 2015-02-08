88de507b8261db1a6cc888c917aa9ad73f51aabc
bkoning23 <koningb@mail.gvsu.edu>
2015-02-08 14:38:45 -0500

    Worked a bit

--- project2.c
+++ project2.c
@@ -4,48 +4,96 @@
 #include <signal.h>
 #include <time.h>
 #include <string.h>
+#include <sys/prctl.h>
+#define SIZE 10
 
 void sigHandler (int);
 void createServer(int, int, char*, char*[], int);
 pid_t pid, secondPid, ppid;
 
+
+struct serverProcess{
+	int min;
+	int max;
+	int current;
+	char name[50];
+	pid_t pid;
+	pid_t children[10];
+	
+} allServers[SIZE];
+
+
 char str[50];
 char *word;
 int minProc, maxProc;
 int var2;
+int currentServer = 0;
 
 
 int main(int argc, char *argv[])
 {
-	printf("Specify Command:\n");
-	fgets(str, 50, stdin);
-	word = strtok(str, " \n");
 	
-	if(!strcmp(word, "createServer")){
-		if ((pid = fork()) < 0){
-			perror("Fork failure");
-			exit(1);
-		}
-		else if (!pid){
+	
+	while(1){
+		printf("Specify Command:\n");
+		fgets(str, 50, stdin);
+		word = strtok(str, " \n");
+		
+		if(!strcmp(word, "createServer")){
+			
 			word = strtok(NULL, " \n");
 			sscanf (word, "%d", &minProc);
 			word = strtok(NULL, " \n");
 			sscanf (word, "%d", &maxProc);		
 			word = strtok(NULL, " \n");
-			printf("Pid: %d\n", getpid());
-			sprintf(argv[0], "%s", word);
-			printf("PARENT SERVER PROCESS: %d\n", getpid());
-			createServer(minProc, maxProc, word, argv, argc);
+			if ((pid = fork()) < 0){
+				perror("Fork failure");
+				exit(1);
+			}
+			//Main Server Process
+			else if (!pid){
+				//Changes the name of the child process to the new name
+				sprintf(argv[0], "%s", word);
+				printf("PARENT SERVER PROCESS: %d\n", getpid());
+				currentServer++;
+				createServer(minProc, maxProc, word, argv, argc);
+				pause();
+				
+			}
+			//Process Manager
+			else{
+				allServers[currentServer].min = minProc;
+				allServers[currentServer].max = maxProc;
+				allServers[currentServer].current = minProc;
+				strcpy(allServers[currentServer].name, word);
+				allServers[currentServer].pid = pid;
+				currentServer++;
+				printf("PROCESS MANAGER: %d\n", getpid());
+				printf("%d\n", allServers[0].min);
+				printf("%d\n", allServers[0].max);
+				printf("%d\n", allServers[0].current);
+				printf("%s\n", allServers[0].name);
+				printf("%d\n", allServers[0].pid);
+			}
 		}
-		else{
-			sleep(10);
-			printf("PROCESS MANAGER: %d\n", getpid());
+		else if (!strcmp(word, "abortServer")){
+			int i = 0;
+			word = strtok(NULL, " \n");
+			while( i < currentServer){
+				if(!strcmp(word, allServers[i].name)){
+					printf("We found a server with the name %s, PID %d. Aborting.\n", word, allServers[i].pid);
+					kill(allServers[i].pid, SIGUSR1);
+					kill(allServers[i].pid, SIGKILL);
+				}
+				i++;
+			}
+			printf("End of Abort");
 		}
-	}
 
+	}
+	
 	return(0);
 		
-
 }
 
 
@@ -54,6 +102,7 @@ int main(int argc, char *argv[])
 void createServer(int minProcs, int maxProcs, char *serverName, char *argv[], int argc){
 	//Add checking that min is less than max
 	//TODO: This still doesn't work right
+		
 	if(minProcs >= maxProcs){
 		perror("Min procs must be less than max procs");
 		exit(0);
@@ -64,48 +113,35 @@ void createServer(int minProcs, int maxProcs, char *serverName, char *argv[], in
 		if ((pid = fork()) < 0){
 			perror("Fork error");
 		}
+		//Server child clone
 		else if(!pid){
 			sprintf(argv[0], "%s %d", word, i);
-			break;
+			prctl(PR_SET_PDEATHSIG, SIGINT);
+			pause();
 		}
+		//Main Server
 		else{
+
 			i++;
 		}
 	}
-			
+	
+		
 	printf("Pid: %d\n", getpid());	
-	sleep(20);
+	signal(SIGUSR1, sigHandler);
+	pause();
 	
 }	
 
-
-
-
-
-/*
 void
 sigHandler (int sigNum)
 {
 	if(sigNum == SIGUSR1){
-		printf("received a SIGUSR1 signal.\n");
-		signal(SIGUSR1, sigHandler);
+		printf("SIGUSER1");
+		exit(0);
 	}
-	if(sigNum == SIGUSR2){
-		printf("received a SIGUSR2 signal.\n");
-		signal(SIGUSR2, sigHandler);
-	}
-	if(sigNum == SIGINT){
-		//If the child receives the signal it does nothing. 
-		if (!firstPid){				
-		}
-		//Parent prints that it is closing and kills the child process 
-		else{
-			printf("received. Closing. \n");
-			kill(firstPid, SIGKILL);
-			exit(0);
-		}
-	}
+
 	
     
 } 
-*/
+
